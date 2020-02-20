e47f755fb8bc1449cae61c8e153873ede0b56ac0
itjjang <freefivez@naver.com>
2020-02-21 02:46:28 +0900

    #2-6 finished

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ const handleListening = () => console.log(`Listening on: http://localhost:${PORT
 const handleHome = (req, res) => res.end("Hi from Home!!");
 const handleProfile = (req, res) => res.send("You are on my profile");
 
+const betweenHome = (req, res, next) => {
+    console.log("Between");
+    next();
+}
+
+app.use(betweenHome);
+
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
 app.listen(PORT, handleListening);
