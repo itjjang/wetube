12dce3fef9d76c56add314fe5fadee9b6b2b5043
itjjang <freefivez@naver.com>
2020-02-22 02:15:33 +0900

    #2-8 finished

--- app.js
+++ app.js
@@ -4,11 +4,9 @@ import morgan from "morgan";
 import helmet from "helmet";
 import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
+import { userRouter } from "./router";
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
 const handleHome = (req, res) => res.end("Hi from Home!!");
 const handleProfile = (req, res) => res.send("You are on my profile");
 
@@ -18,14 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));     // tiny, combined, common, short, dev
 
-/*
-const middleware = (req, res, next) => {
-//    res.send("not happening");
-    next();
-}
-*/
-
-//app.get("/", middleware, handleHome);
 app.get("/", handleHome);
 app.get("/profile", handleProfile);
-app.listen(PORT, handleListening);
+app.use("/user", userRouter)
+
+export default app;
