2a7ddacedd2575cc27f1fd1fe952b6b062cef25c
itjjang <freefivez@naver.com>
2020-02-22 03:04:06 +0900

    #2-10 finished

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,11 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.home,           (req, res) => res.send("Users"));
+userRouter.get(routes.userDetail,     (req, res) => res.send("UserDetail"));
+userRouter.get(routes.userProfile,    (req, res) => res.send("UserProfile"));
+userRouter.get(routes.changePassword, (req, res) => res.send("UserChangePassword"));
 
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -1,5 +1,12 @@
 import express from "express";
+import routes from "../routes";
 
 const videoRouter = express.Router();
+
+videoRouter.get(routes.home,      (req, res) => res.send("Videos"));
+videoRouter.get(routes.upload,      (req, res) => res.send("VideoUpload"));
+videoRouter.get(routes.videoDetail, (req, res) => res.send("VideoDetail"));
+videoRouter.get(routes.editVideo,   (req, res) => res.send("VideoEdit"));
+videoRouter.get(routes.deleteVideo, (req, res) => res.send("VideoDelete"));
 
 export default videoRouter;
