b008df4e0a79376a836d4148382f54a75aef5676
itjjang <freefivez@naver.com>
2020-02-25 03:40:07 +0900

    #2-17 finished

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,7 @@
-export const joinController = (req, res) => res.render("join");
-export const loginController = (req, res) => res.render("login");
-export const logoutController = (req, res) => res.render("logout");
+export const joinController = (req, res) => res.render("join", { pageTitle: "Join" });
+export const loginController = (req, res) => res.render("login", { pageTitle: "Login" });
+export const logoutController = (req, res) => res.render("logout", { pageTitle: "Logout" });
 //export const usersController = (req, res) => res.render("Users");
-export const userDetailController = (req, res) => res.send("userDetail");
-export const editProfileController = (req, res) => res.send("editProfile");
-export const changePasswordController = (req, res) => res.send("changePassword");
+export const userDetailController = (req, res) => res.send("userDetail", { pageTitle: "User Detail" });
+export const editProfileController = (req, res) => res.send("editProfile", { pageTitle: "Edit Profile" });
+export const changePasswordController = (req, res) => res.send("changePassword", { pageTitle: "Change Password" });
--- controller/videoController.js
+++ controller/videoController.js
@@ -1,8 +1,8 @@
 //export const homeController = (req, res) => res.send("Home");
-export const homeController = (req, res) => res.render("home");
-export const searchController = (req, res) => res.render("search");
+export const homeController = (req, res) => res.render("home", { pageTitle: "Home" });
+export const searchController = (req, res) => res.render("search", { pageTitle: "Search" });
 //export const videosController = (req, res) => res.send("Videos");
-export const uploadVideoController = (req, res) => res.render("upload");
-export const videoDetailController = (req, res) => res.render("videoDetail");
-export const editVideoController = (req, res) => res.render("editVideo");
-export const deleteVideoController = (req, res) => res.render("deleteVideo");
+export const uploadVideoController = (req, res) => res.render("upload", { pageTitle: "Upload" });
+export const videoDetailController = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const editVideoController = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+export const deleteVideoController = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
