4a1888cffd4ab22edcbb206d29c2f7c656a4878d
itjjang <freefivez@naver.com>
2020-02-28 21:53:10 +0900

    #2-18 finished

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,8 +1,17 @@
 //export const homeController = (req, res) => res.send("Home");
 export const homeController = (req, res) => res.render("home", { pageTitle: "Home" });
-export const searchController = (req, res) => res.render("search", { pageTitle: "Search" });
+
+export const searchController = (req, res) => {
+    const { query: { term: searchingBy } } = req;
+    res.render("search", { pageTitle: "Search", searchingBy });
+}
+
 //export const videosController = (req, res) => res.send("Videos");
+
 export const uploadVideoController = (req, res) => res.render("upload", { pageTitle: "Upload" });
+
 export const videoDetailController = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+
 export const editVideoController = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
+
 export const deleteVideoController = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
