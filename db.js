1a6d2a72f85131a08d8ab91c38a7b1f255e8d0ee
itjjang <freefivez@naver.com>
2020-02-29 01:57:17 +0900

    #2-22 finished

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ export const videos =  [
         id: 324393,
         title: "Video awesome",
         description: "This is something I love",
-        vuews: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Nicolas",
@@ -15,8 +15,8 @@ export const videos =  [
         id: 1212121,
         title: "Video Super",
         description: "This is something I love",
-        vuews: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Nicolas",
@@ -27,8 +27,8 @@ export const videos =  [
         id: 55555,
         title: "Video nice",
         description: "This is something I love",
-        vuews: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Nicolas",
@@ -39,8 +39,8 @@ export const videos =  [
         id: 11111,
         title: "Video perfect",
         description: "This is something I love",
-        vuews: 24,
-        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        views: 24,
+        videoFile: "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
         creator: {
             id: 121212,
             name: "Nicolas",
