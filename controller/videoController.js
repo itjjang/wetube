674ed860ff9444bd6fc6d37bb12d99cdae0d754b
itjjang <freefivez@naver.com>
2020-03-06 23:16:28 +0900

    #3-8 finished

--- controller/videoController.js
+++ controller/videoController.js
@@ -4,7 +4,6 @@ import Video from "../models/Video";
 export const homeController = async(req, res) => {
     try {
         const videos = await Video.find({});
-        console.log(videos);
         res.render("home", { pageTitle: "Home", videos });
     } catch(error) {
         console.log(error);
@@ -24,11 +23,21 @@ export const postUploadVideoController = async(req, res) => {
         fileUrl: path,
         title,
         description
-    });
-    console.log(newVideo)
+    });    
     res.redirect(routes.videoDetail(newVideo.id));
 }
 
-export const videoDetailController = (req, res) => res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetailController = async(req, res) => {
+    const { params: { id } } = req;
+    try {
+        const video = await Video.findById(id);    
+        console.log(video);
+        res.render("videoDetail", { pageTitle: "Video Detail", video });
+    } catch(error) {
+        console.log(error);
+        res.redirect(routes.home);
+    }    
+}
+
 export const editVideoController = (req, res) => res.render("editVideo", { pageTitle: "Edit Video" });
 export const deleteVideoController = (req, res) => res.render("deleteVideo", { pageTitle: "Delete Video" });
